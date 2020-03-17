1287811c66cf0be128a8ceff590771399049e539
xiekch <xiekch@qq.com>
2020-03-17 16:21:46 +0800

    stggered grid recyclerview

---
+++ app/src/main/java/com/example/myapplication/recyclerView/StaggeredGridRecyclerViewAdapter.java
@@ -0,0 +1,73 @@
+package com.example.myapplication.recyclerView;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+
+public class StaggeredGridRecyclerViewAdapter extends RecyclerView.Adapter<StaggeredGridRecyclerViewAdapter.MyViewHolder> {
+    private Context mContext;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public ImageView imageView;
+
+        public MyViewHolder(ImageView v) {
+            super(v);
+            imageView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public StaggeredGridRecyclerViewAdapter(Context context) {
+        mContext = context;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public StaggeredGridRecyclerViewAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                                            int viewType) {
+        // create a new view
+        ImageView v = (ImageView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.staggered_item_imageview, parent, false);
+//        ...
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        if (position % 2 == 0) {
+            holder.imageView.setImageResource(R.mipmap.flower);
+        } else {
+            holder.imageView.setImageResource(R.mipmap.year2020);
+        }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, String.valueOf(position), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return 40;
+    }
+
+}
