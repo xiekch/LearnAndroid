84542f86e9d51c6b4258dce6de98f34dbc10e029
xiekch <xiekch@qq.com>
2020-04-08 21:00:28 +0800

    intent

--- app/src/main/java/com/example/myapplication/intent/IntentActivity.java
+++ app/src/main/java/com/example/myapplication/intent/IntentActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,9 +16,12 @@ import android.widget.Toast;
 
 import com.example.myapplication.R;
 
-public class IntentActivity extends AppCompatActivity {
+public class IntentActivity extends AppCompatActivity implements View.OnClickListener {
     private Button jump_button1;
     private Button jump_button2;
+    private Button jump_button3;
+    private Button jump_button4;
+    private Button share_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +31,51 @@ public class IntentActivity extends AppCompatActivity {
         logTaskName();
 
         jump_button1 = findViewById(R.id.jump_button1);
-
-        jump_button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction("android.intent.action.Jump");
-                startActivityForResult(intent, 0);
-            }
-        });
-
         jump_button2 = findViewById(R.id.jump_button2);
-        jump_button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(IntentActivity.this, IntentActivity.class);
-                startActivity(intent);
-            }
-        });
+        jump_button3 = findViewById(R.id.jump_button3);
+        jump_button4 = findViewById(R.id.jump_button4);
+        share_button = findViewById(R.id.share_button);
+
+        jump_button1.setOnClickListener(this);
+        jump_button2.setOnClickListener(this);
+        jump_button3.setOnClickListener(this);
+        jump_button4.setOnClickListener(this);
+        share_button.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.jump_button1:
+                Intent intent1 = new Intent();
+                intent1.setAction("android.intent.action.Jump");
+                startActivityForResult(intent1, 0);
+                break;
+            case R.id.jump_button2:
+                Intent intent2 = new Intent(IntentActivity.this, IntentActivity.class);
+                startActivity(intent2);
+                break;
+            case R.id.jump_button3:
+                Intent read1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.runoob.com"));
+                startActivity(read1);
+                break;
+            case R.id.jump_button4:
+                Uri uri = Uri.parse("tel:10086");
+                Intent call = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(call);
+                break;
+            case R.id.share_button:
+                Intent share_intent = new Intent();
+                share_intent.setAction(Intent.ACTION_SEND); // set action
+                share_intent.setType("text/plain"); // set the type to share
+                share_intent.putExtra(Intent.EXTRA_SUBJECT, "share"); //add subject
+                share_intent.putExtra(Intent.EXTRA_TEXT, "share with you:" + "android"); //add content
+                share_intent = Intent.createChooser(share_intent, "share");
+                startActivity(share_intent);
+                break;
+
+        }
     }
 
     @Override
