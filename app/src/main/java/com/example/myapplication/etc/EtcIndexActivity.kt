a31618ee608b9aa5177497a244d94556c7fa0b09
xiekch <xiekch@qq.com>
2021-06-04 11:15:33 +0800

    RTL language

--- app/src/main/java/com/example/myapplication/etc/EtcIndexActivity.kt
+++ app/src/main/java/com/example/myapplication/etc/EtcIndexActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.etc
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore.ACTION_IMAGE_CAPTURE
+import android.text.BidiFormatter
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,12 +13,19 @@ import kotlinx.android.synthetic.main.activity_etc_index.*
 
 class EtcIndexActivity : AppCompatActivity(), View.OnClickListener {
     private val CAMERA = 1
+
+    companion object {
+        const val TAG = "EtcIndexActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_etc_index)
 
         buttonOpenCamera.setOnClickListener(this)
         buttonMusicPlayer.setOnClickListener(this)
+        buttonRTLLanguage.setOnClickListener(this)
+        buttonTest.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -29,6 +37,9 @@ class EtcIndexActivity : AppCompatActivity(), View.OnClickListener {
                 )
             }
             R.id.buttonMusicPlayer -> startActivity(Intent(this, MusicPlayerActivity::class.java))
+            R.id.buttonRTLLanguage -> rTLLanguage()
+            R.id.buttonTest -> {
+            }
         }
     }
 
@@ -37,5 +48,26 @@ class EtcIndexActivity : AppCompatActivity(), View.OnClickListener {
         if (requestCode == this.CAMERA && resultCode == RESULT_OK && data != null) {
             Toast.makeText(this, "a photo taken", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun rTLLanguage() {
+        val mySuggestion = "15 Bay Street, Laurel, CA"
+        val myBidiFormatter: BidiFormatter = BidiFormatter.getInstance()
+
+        // The "did_you_mean" localized string resource includes
+        // a "%s" placeholder for the suggestion.
+        Toast.makeText(
+            this,
+            String.format(getString(R.string.did_you_mean), mySuggestion),
+            Toast.LENGTH_SHORT
+        ).show()
+        Toast.makeText(
+            this,
+            String.format(
+                getString(R.string.did_you_mean),
+                myBidiFormatter.unicodeWrap(mySuggestion)
+            ),
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
