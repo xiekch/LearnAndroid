6ada7eb76dfae84b2701add5572e52e26e196437
xiekch <xiekch@qq.com>
2021-06-05 10:23:47 +0800

    onTouch

--- app/src/main/java/com/example/myapplication/etc/EtcIndexActivity.kt
+++ app/src/main/java/com/example/myapplication/etc/EtcIndexActivity.kt
@@ -4,14 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore.ACTION_IMAGE_CAPTURE
 import android.text.BidiFormatter
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 import com.example.myapplication.etc.musicplayer.MusicPlayerActivity
 import kotlinx.android.synthetic.main.activity_etc_index.*
+import kotlinx.android.synthetic.main.activity_thread_index.*
 
-class EtcIndexActivity : AppCompatActivity(), View.OnClickListener {
+class EtcIndexActivity : AppCompatActivity(), View.OnClickListener, View.OnTouchListener {
     private val CAMERA = 1
 
     companion object {
@@ -25,7 +28,7 @@ class EtcIndexActivity : AppCompatActivity(), View.OnClickListener {
         buttonOpenCamera.setOnClickListener(this)
         buttonMusicPlayer.setOnClickListener(this)
         buttonRTLLanguage.setOnClickListener(this)
-        buttonTest.setOnClickListener(this)
+        textMove.setOnTouchListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -69,5 +72,36 @@ class EtcIndexActivity : AppCompatActivity(), View.OnClickListener {
             ),
             Toast.LENGTH_SHORT
         ).show()
+    }
+
+
+    var preX = 0
+    var preY = 0
+    override fun onTouch(v: View, event: MotionEvent?): Boolean {
+        if (event == null) return false
+        Log.i(TAG, "action ${event.action}")
+        val x = event.rawX.toInt()
+        val y = event.rawY.toInt()
+//        Log.i(TAG, "scroll ${v.scrollX} ${v.scrollY}")
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                preX = x
+                preY = y
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val deltaX = x - preX
+                val deltaY = y - preY
+                Log.i(TAG, "$x $y -- $deltaX $deltaY -- $preX $preY")
+                v.layout(v.left + deltaX, v.top + deltaY, v.right + deltaX, v.bottom + deltaY)
+//                (v.parent as View).scrollBy(-deltaX,-deltaY)
+                preX = x
+                preY = y
+                return true
+            }
+            MotionEvent.ACTION_UP -> {
+//                Log.i(TAG, "scroll ${(v.parent as View).scrollX} ${(v.parent as View).scrollY}")
+            }
+        }
+        return true
     }
 }
