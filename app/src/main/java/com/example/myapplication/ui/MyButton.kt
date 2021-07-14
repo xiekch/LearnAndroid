a0f0a3c8ac5e99ca84d515405e6d0e4a86a09dc4
xiekch <xiekch@qq.com>
2021-07-14 14:58:21 +0800

    MyButton

--- app/src/main/java/com/example/myapplication/ui/MyButton.kt
+++ app/src/main/java/com/example/myapplication/ui/MyButton.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication.ui
 
 import android.content.Context
+import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
@@ -13,12 +14,27 @@ class MyButton @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = R.attr.buttonStyle
 ) : AppCompatButton(context, attrs, defStyleAttr) {
-
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        super.onTouchEvent(event)
-        Toast.makeText(context, "onTouchEvent", Toast.LENGTH_SHORT).show()
-        Log.d("MyButton", "onTouchEvent")
-        return false
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.GRAY
+        style = Paint.Style.FILL
     }
 
+    private val rect = Rect()
+    private val bitmap =
+        BitmapFactory.decodeResource(resources, com.example.myapplication.R.mipmap.flower)
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        Toast.makeText(context, "onTouchEvent", Toast.LENGTH_SHORT).show()
+        Log.d("MyButton", "onTouchEvent")
+        return super.onTouchEvent(event)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        if (canvas == null) return
+        super.onDraw(canvas)
+        canvas.drawCircle(10f, 10f, 30f, paint)
+        canvas.drawCircle(width - 10f, height - 10f, 30f, paint)
+        rect.set(0, 0, 50, 50)
+        canvas.drawBitmap(bitmap, null, rect, null)
+    }
 }
